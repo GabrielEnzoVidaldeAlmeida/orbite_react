99f3036e29d7561b3db2d6c89238eb1caaf6d83e
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-04-23 12:42:59 -0300

    adicionando um icone de olho na página login

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import styles from './styles.module.css'
+import { Eye, EyeOff } from 'lucide-react';
 
 type DefaultInputProps = {
   id: string;
@@ -6,10 +8,23 @@ type DefaultInputProps = {
 } & React.ComponentProps<'input'>;
 
 export function DefaultInput ({ id, labelText, type, ...rest }: DefaultInputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = type === "password";
+  const inputType = isPassword && showPassword ? "text" : type;
+
   return (
     <div className={styles.inputWrapper}>
       {labelText && <label htmlFor={id}>{labelText}</label>}
-      <input className={styles.input} id={id} type={type} {...rest} />
+      <div className={styles.inputContainer}>
+        <input className={styles.input} id={id} type={inputType} {...rest} />
+
+        {isPassword && (
+          <button type="button" className={styles.toggleBtn} onClick={() => setShowPassword(prev => !prev)}>
+            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+          </button>
+        )}
+      </div>
     </div>
   )
 }
