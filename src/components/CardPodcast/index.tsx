b9f5a8e00f5cf2e5ed44651fd6bf3f0fd03fc75c
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-04-28 11:27:31 -0300

    finalizando o card podcast da home

--- src/components/CardPodcast/index.tsx
+++ src/components/CardPodcast/index.tsx
@@ -8,10 +8,16 @@ import capdo_75 from '../../assets/images/img-podcasts/img-eps/capdo_75.jpg'
 import capdo_81 from '../../assets/images/img-podcasts/img-eps/capdo_81.png'
 import capdo_85 from '../../assets/images/img-podcasts/img-eps/capdo_85.png'
 
-
-
 // OTHERS
 import CardEpMini from '../CardEpMini'
+
+// type CardPodcastProps = {
+//   img: string,
+//   title: string,
+//   followers: number,
+//   description: string,
+
+// }
 
 export function CardPodcast () {
   return (
@@ -29,19 +35,20 @@ export function CardPodcast () {
             <span>Ciência Ao Pé Do Ouvido</span> 
             <label>123.987 seguidores</label>
           </h2>
-          <p><strong>Descrição:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at euismod ipsum. Aenean eget sapien nulla. Praesent ultrices volutpat bibendum. Donec vestibulum eget neque sit amet porta. Duis eget dui est.  </p>
+          <p><strong>Descrição:</strong> Ciência Ao Pé Do Ouvido explora o fascinante mundo da ciência de maneira simples e acessível. Cada episódio traz descobertas, curiosidades e histórias inspiradoras, com o objetivo de desmistificar temas complexos e aproximar a ciência do nosso cotidiano.</p>
 
           
           <h3>Episódios Recentes:</h3>
           <div className={styles.ratedPodcast}>
-            <CardEpMini title='Juventude e Ciência' img={capdo_75} />
-            <CardEpMini title='Luta Antimanicomial' img={capdo_81} />
-            <CardEpMini title='Eu estou tão cansado, mas não pra dizer' img={capdo_85} />
+            <CardEpMini title='#75 Juventude e Ciência' img={capdo_75} />
+            <CardEpMini title='#81 Luta Antimanicomial' img={capdo_81} />
+            <CardEpMini title='#85 Eu estou tão cansado, mas não pra dizer' img={capdo_85} />
           </div>
-
-
-
         </div>
+
+          <div className={styles.linkPage}>
+            <a href='#'>Acessar página do podcast</a>
+          </div>
       </div>
     </div>
   )
